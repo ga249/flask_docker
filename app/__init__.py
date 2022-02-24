52122b3893208deb41cdcd421fd7fe0d7df379c6
ga249 <ga249@njit.edu>
2022-02-24 12:30:43 -0500

    added simple pages and calculator,homepage, and simple pages tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 """A simple flask web app"""
 from flask import Flask
+from app.simple_pages import simple_pages
+
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    app.register_blueprint(simple_pages)
 
-    @app.route("/")
-    def index():
-        return "Hello, World! s"
+    #@app.route("/")
+    #def index():
+    #    return "Hello, World! s"
 
     return app
